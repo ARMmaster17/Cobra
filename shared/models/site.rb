55ec34ce4afc18fff6c0aaf16f2509844e259fa6
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-09-03 01:31:33 +0000

    Added real data to Site and Zone pages.

--- shared/models/site.rb
+++ shared/models/site.rb
@@ -1,3 +1,17 @@
 class Site < ActiveRecord::Base
     has_many :zones, dependent: :destroy
+    def total_spaces
+        count = 0
+        self.zones.each do |z|
+            count += z.total_spaces
+        end
+        return count
+    end
+    def used_spaces
+        count = 0
+        self.zones.each do |z|
+            count += z.used_spaces
+        end
+        return count
+    end
 end
