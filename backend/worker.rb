cf624ad6e1ada8ff466a19b3c06321e752407202
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-09-12 22:50:40 +0000

    Fixed CC QA check.

--- backend/worker.rb
+++ backend/worker.rb
@@ -13,7 +13,6 @@ ch = conn.create_channel
 q = ch.queue("cobra.outbound")
 
 q.subscribe(:block => true) do |delivery_info, metadata, payload|
-    data = Hash.new
     data = JSON.parse(payload)
     Processor.go(data)
     #ch.ack(delivery_info.delivery_tag)
